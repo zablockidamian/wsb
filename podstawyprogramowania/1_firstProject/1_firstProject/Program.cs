be8aa445e420dc3ecee8f4240753101f4eda100b
damizablocki99@gmail.com <damizablocki99@gmail.com>
2019-10-05 11:30:12 +0200

    ZablockiDamian

---
+++ podstawyprogramowania/1_firstProject/1_firstProject/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _1_firstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("#");
+            Console.WriteLine("##");
+            Console.WriteLine("###");
+            /*
+             *  #
+             *  ##
+             *  ###
+             */
+
+            string name;
+            name = "Anna";
+            Console.WriteLine(name);
+            Console.WriteLine("Masz na imie: " + name);
+            Console.WriteLine("Masz na imie: {0}", name);
+            Console.ReadKey();
+
+        }
+    }
+}
